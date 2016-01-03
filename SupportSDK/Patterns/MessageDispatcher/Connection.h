3de85d62acc373e1c885e7eadc185e1881674af2
Ivan Shcherbina <jester.92@yandex.ua>
2016-01-04 01:08:54 +0300

    [Patterns] Connection must not have copy constructor (in destructor of temporary object mp_dispatcher->Unregister is called)

--- SupportSDK/Patterns/MessageDispatcher/Connection.h
+++ SupportSDK/Patterns/MessageDispatcher/Connection.h
@@ -14,10 +14,30 @@ namespace SDK
 		std::string m_publisher;
 
 	public:
+		Connection(const Connection&) = delete;
+		Connection& operator = (const Connection&) = delete;
+
 		Connection()
 			: m_type(typeid(*this))
 			, mp_dispatcher(nullptr)
 		{}
+
+		Connection(Connection&& right)
+			: mp_dispatcher(right.mp_dispatcher)
+			, m_publisher(right.m_publisher)
+			, m_type(right.m_type)
+		{
+			right.mp_dispatcher = nullptr;
+		}
+
+		Connection& operator = (Connection&& right)
+		{
+			mp_dispatcher = right.mp_dispatcher;
+			m_publisher = right.m_publisher;
+			m_type = right.m_type;
+			right.mp_dispatcher = nullptr;
+			return *this;
+		}
 
 		template <typename HandlerType, typename EventType>
 		Connection(MessageDispatcher& o_dispatcher, HandlerType& i_instance, void (HandlerType::*member_function)(const EventType&), const std::string& i_publisher)
